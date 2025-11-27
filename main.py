4b69e6a37d4683bdd77334724b16bcf83392986d
rayler <121004818+raylerr481@users.noreply.github.com>
2025-11-27 16:48:51 -0300

    Rename CalcLayout to CalculatorWindow

--- main.py
+++ main.py
@@ -1,26 +1,12 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 
-class CalcLayout(BoxLayout):
-    def press_button(self, label):
-        if label == "C":
-            self.ids.input.text = ""
-            return
-        
-        if label == "=":
-            try:
-                expr = self.ids.input.text
-                result = eval(expr)
-                self.ids.input.text = str(result)
-            except:
-                self.ids.input.text = "Error"
-            return
-
-        self.ids.input.text += label
+class CalculatorWindow(BoxLayout):
+    pass
 
 class CalculatorApp(App):
     def build(self):
-        return CalcLayout()
+        return CalculatorWindow()
 
 if __name__ == "__main__":
     CalculatorApp().run()
